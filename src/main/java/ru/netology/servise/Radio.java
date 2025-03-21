8e1d7db81fc3081b3f772eefa05382a415a2b62c
Asja <anastasjamedved@gmail.com>
2025-03-21 20:18:18 +0300

    pull commit

---
+++ src/main/java/ru/netology/servise/Radio.java
@@ -0,0 +1,76 @@
+package ru.netology.servise;
+
+public class Radio {
+    // ЗВУК
+
+    private int currentVolume;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    // public void setToMaxVolume() {
+    //     currentVolume = 100;
+    //  }
+
+    // ВОЛНА
+
+    private int currentWave;
+
+    public int getCurrentWave() {
+        return currentWave;
+    }
+
+    public void setCurrentWave(int newCurrentWave) {
+        if (newCurrentWave < 0) {
+            currentWave = 9;
+        }
+        if (newCurrentWave > 9) {
+            currentWave = 0;
+        }
+        currentWave = newCurrentWave;
+    }
+
+
+    public void next() {
+        if (currentWave < 9) {
+            currentWave = currentWave + 1;
+        } else {
+            currentWave = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentWave > 0) {
+            currentWave = currentWave - 1;
+        } else {
+            currentWave = 9;
+        }
+    }
+    //  public void setToMaxWave () {
+    //    currentWave = 9;
+    // }
+}
---
+++ src/test/java/ru/netology/servise/RadioTest.java
@@ -0,0 +1,117 @@
+package ru.netology.servise;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    public void setValidVolume() {  //актуальный звук
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+        int expected = 50;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void setMaxVolume() { // установка макс звука
+        Radio radio = new Radio();
+        radio.setCurrentVolume(101);
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setVolumeToOneMore() {  // увеличение звука на +1
+        Radio radio = new Radio();
+        radio.setCurrentVolume(75);
+        radio.increaseVolume();
+        int expected = 76;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setMoreMaxVolume() { // переключение больше макс на +1
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+        radio.increaseVolume();
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setVolumeToOneLower() { // уменьшение звука на -1
+        Radio radio = new Radio();
+        radio.setCurrentVolume(36);
+        radio.decreaseVolume();
+        int expected = 35;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setMoreMinVolume() {  // переключение зука больше мин на -1
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+// ВОЛНА
+
+    @Test
+    public void setValidWave() {  // актуальная волна
+        Radio radio = new Radio();
+        radio.setCurrentWave(7);
+        int expected = 7;
+        int actual = radio.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    public void setWaveToOneMore() {  // увеличение волны на +1
+        Radio radio = new Radio();
+        radio.setCurrentWave(5);
+        radio.next();
+        int expected = 6;
+        int actual = radio.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextWaveFromMaxToMin() {  // переход с 9 на 0
+        Radio radio = new Radio();
+        radio.setCurrentWave(9);
+        radio.next();
+        int expected = 0;
+        int actual = radio.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setWaveToOneLower() { // уменьшение волны на -1
+        Radio radio = new Radio();
+        radio.setCurrentWave(9);
+        radio.prev();
+        int expected = 8;
+        int actual = radio.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevWaveFromMinToMax() { // переход с 0 на 9
+        Radio radio = new Radio();
+        radio.setCurrentWave(0);
+        radio.prev();
+        int expected = 9;
+        int actual = radio.getCurrentWave();
+        Assertions.assertEquals(expected, actual);
+    }
+}
